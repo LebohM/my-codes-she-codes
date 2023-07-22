586ead9bddedd5c7727c032c2deaae5038a54df9
LebohM <46931086+LebohM@users.noreply.github.com>
2023-07-22 20:29:42 +0200

    added innerHTML to HTML elements

--- Weather App Project/src/index.js
+++ Weather App Project/src/index.js
@@ -1,3 +1,23 @@
+function displayTemp(response){
+console.log(response);
+let Currentcity = document.querySelector("#city");
+Currentcity.innerHTML = response.data.name;
+
+let DescribeTemp = document.querySelector("#description");
+DescribeTemp.innerHTML = response.data.weather[0].description;
+
+let temperature = document.querySelector("#temp");
+temperature.innerHTML = Math.round(response.data.main.temp);
+
+let humid = document.querySelector("#humidity");
+humid.innerHTML = Math.round(response.data.main.humidity);
+
+let windSpeed = document.querySelector("#wind");
+windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
 let Apikey = "1f151e560699fdd0aaf7cad161399047";
 const ApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=new york&appid=${Apikey}&units=metric`;
-console.log(ApiUrl);
+
+axios.get(ApiUrl).then(displayTemp);
